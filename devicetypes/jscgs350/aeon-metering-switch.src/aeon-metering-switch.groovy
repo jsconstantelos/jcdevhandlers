40224881b65e3db24e0c2c79a984ad34d8b976a9
constjs <constjs@users.noreply.github.com>
2016-02-17 17:36:45 -0500

    Update aeon-metering-switch.groovy

--- devicetypes/jscgs350/aeon-metering-switch.src/aeon-metering-switch.groovy
+++ devicetypes/jscgs350/aeon-metering-switch.src/aeon-metering-switch.groovy
@@ -20,7 +20,7 @@
  */
 metadata {
 	// Automatically generated. Make future change here.
-	definition (name: "My Aeon Metering Switch v2", namespace: "jscgs350", author: "SmartThings") {
+	definition (name: "My Aeon Metering Switch", namespace: "jscgs350", author: "SmartThings") {
 		capability "Energy Meter"
 		capability "Actuator"
 		capability "Switch"
