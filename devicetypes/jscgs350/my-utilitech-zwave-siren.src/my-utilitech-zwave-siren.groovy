5c34151ec6a87e1db3bf8c718d3533696f4bc7bf
John Constantelos <jsconst@gmail.com>
2016-02-19 16:31:24 -0500

    Initial commit for github integration

---
+++ devicetypes/jscgs350/my-utilitech-zwave-siren.src/my-utilitech-zwave-siren.groovy
@@ -0,0 +1,176 @@
+/**
+ *  Utilitech/Everspring Siren
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License. You may obtain a copy of the License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ *  for the specific language governing permissions and limitations under the License.
+ *
+ *  Version: v2
+ *
+ *  Updates:
+ *  -------
+ *  02-19-2016 : Initial commit
+ *
+ */
+metadata {
+	definition (name: "My Utilitech Z-Wave Siren", namespace: "jscgs350", author: "SmartThings") {
+		capability "Actuator"
+        capability "Alarm"
+        capability "Battery"
+        capability "Polling"
+        capability "Refresh"
+        capability "Sensor"
+		capability "Switch"
+		attribute "alarmState", "string"
+
+		fingerprint inClusters: "0x20,0x25,0x86,0x80,0x85,0x72,0x71"
+	}
+
+	tiles(scale: 2) {
+		multiAttributeTile(name:"alarm", type: "lighting", width: 6, height: 4, canChangeIcon: true){
+			tileAttribute ("device.alarm", key: "PRIMARY_CONTROL") {
+				attributeState "both", label:'alarm!', action:'alarm.off', icon:"st.alarm.alarm.alarm", backgroundColor:"#e86d13"
+				attributeState "off", label:'off', action:'alarm.strobe', icon:"st.alarm.alarm.alarm", backgroundColor:"#79b821"
+			}
+            tileAttribute ("statusText", key: "SECONDARY_CONTROL") {
+           		attributeState "statusText", label:'${currentValue}'       		
+            }
+		}
+		standardTile("off", "device.alarm", width: 2, height: 2, inactiveLabel: false, decoration: "flat") {
+			state "default", label:'', action:"alarm.off", icon:"st.secondary.off"
+		}
+        valueTile("battery", "device.battery", width: 3, height: 2, inactiveLabel: false, decoration: "flat") {
+			state "battery", label:'${currentValue}% battery', unit:""
+		}
+        standardTile("refresh", "device.refresh", width: 3, height: 2, inactiveLabel: false, decoration: "flat") {
+			state "default", label:'', action:"refresh.refresh", icon:"st.secondary.refresh"
+		}
+        valueTile("statusText", "statusText", inactiveLabel: false, width: 2, height: 2) {
+			state "statusText", label:'${currentValue}', backgroundColor:"#ffffff"
+		}        
+		main "alarm"
+		details(["alarm","battery","refresh"])
+	}
+}
+
+def parse(String description) {
+	log.debug "parse($description)"
+	def result = null
+	def cmd = zwave.parse(description, [0x20: 1])
+	if (cmd) {
+		result = createEvents(cmd)
+	}
+    
+    def statusTextmsg = ""
+    statusTextmsg = "Siren is ${device.currentState('alarmState').value} (toggle on/off)."
+    sendEvent("name":"statusText", "value":statusTextmsg)
+//    log.debug statusTextmsg
+
+	log.debug "Parse returned ${result?.descriptionText}"
+	return result
+}
+
+def createEvents(physicalgraph.zwave.commands.batteryv1.BatteryReport cmd) {
+	def map = [ name: "battery", unit: "%" ]
+	if (cmd.batteryLevel == 0xFF) {
+		map.value = 1
+		map.descriptionText = "$device.displayName has a low battery"
+	} else {
+		map.value = cmd.batteryLevel
+	}
+	state.lastbatt = new Date().time
+	createEvent(map)
+}
+
+def poll() {
+	if (secondsPast(state.lastbatt, 36*60*60)) {
+		return zwave.batteryV1.batteryGet().format
+	} else {
+		return null
+	}
+}
+
+private Boolean secondsPast(timestamp, seconds) {
+	if (!(timestamp instanceof Number)) {
+		if (timestamp instanceof Date) {
+			timestamp = timestamp.time
+		} else if ((timestamp instanceof String) && timestamp.isNumber()) {
+			timestamp = timestamp.toLong()
+		} else {
+			return true
+		}
+	}
+	return (new Date().time - timestamp) > (seconds * 1000)
+}
+
+def on() {
+	log.debug "sending on"
+	[
+		zwave.basicV1.basicSet(value: 0xFF).format(),
+		zwave.basicV1.basicGet().format()
+	]
+}
+
+def off() {
+	log.debug "sending off"
+	[
+		zwave.basicV1.basicSet(value: 0x00).format(),
+		zwave.basicV1.basicGet().format()
+	]
+}
+
+def strobe() {
+	log.debug "sending stobe/on command"
+	[
+		zwave.basicV1.basicSet(value: 0xFF).format(),
+		zwave.basicV1.basicGet().format()
+	]
+}
+
+def both() {
+	log.debug "Sending ON command to ${device.displayName} even though BOTH was called"
+	[
+		zwave.basicV1.basicSet(value: 0xFF).format(),
+		zwave.basicV1.basicGet().format()
+	]
+}
+
+def refresh() {
+	log.debug "sending battery refresh command"
+	zwave.batteryV1.batteryGet().format()
+}
+
+def createEvents(physicalgraph.zwave.commands.basicv1.BasicReport cmd)
+{
+	def switchValue = cmd.value ? "on" : "off"
+	def alarmValue
+	if (cmd.value == 0) {
+		alarmValue = "off"
+        sendEvent(name: "alarmState", value: "waiting for events")
+	}
+	else if (cmd.value <= 33) {
+		alarmValue = "strobe"
+        sendEvent(name: "alarmState", value: "ON - strobe only!")
+	}
+	else if (cmd.value <= 66) {
+		alarmValue = "siren"
+        sendEvent(name: "alarmState", value: "ON - siren only!")
+	}
+	else {
+		alarmValue = "both"
+        sendEvent(name: "alarmState", value: "ON - strobe and siren!")
+	}
+	[
+		createEvent([name: "switch", value: switchValue, type: "digital", displayed: false]),
+		createEvent([name: "alarm", value: alarmValue, type: "digital"])
+	]
+}
+
+def createEvents(physicalgraph.zwave.Command cmd) {
+	log.warn "UNEXPECTED COMMAND: $cmd"
+}
