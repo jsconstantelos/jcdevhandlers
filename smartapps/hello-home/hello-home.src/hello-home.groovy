0204d3c2028db771b83aa0a421d86d53c8c1bd0b
constjs <constjs@users.noreply.github.com>
2016-03-17 19:24:43 -0400

    Delete hello-home.groovy

--- smartapps/hello-home/hello-home.src/hello-home.groovy
+++
@@ -1,9 +0,0 @@
-definition(
-    name: "Hello Home",
-    namespace: "Hello Home",
-    author: "SmartThings",
-    description: "Hi",
-    category: "",
-    iconUrl: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience.png",
-    iconX2Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png",
-    iconX3Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png")
