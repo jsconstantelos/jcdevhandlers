32ae276000af63860f80c51c20a8b12f000d1177
constjs <constjs@users.noreply.github.com>
2016-10-29 13:50:53 -0400

    Update and rename thermostat-manager-parent.groovy to thermostat-manager.groovy

--- smartapps/jscgs350/thermostat-manager-parent.src/thermostat-manager.groovy
+++ smartapps/jscgs350/thermostat-manager-parent.src/thermostat-manager.groovy
@@ -1,15 +1,15 @@
 definition(
-    name: "Thermostat Manager Parent",
+    name: "Thermostat Manager",
     singleInstance: true,
     namespace: "jscgs350",
     author: "jscgs350",
-    description: "Thermostat Manager Parent",
+    description: "Thermostat Manager",
     category: "My Apps",
     iconUrl: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience.png",
     iconX2Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png")
 
 preferences {
-    page(name: "mainPage", title: "Thermostat Manager Parent", install: true, uninstall: true,submitOnChange: true) {
+    page(name: "mainPage", title: "Thermostat Manager", install: true, uninstall: true,submitOnChange: true) {
             section {
                     app(name: "childRules", appName: "Thermostat Manager Child", namespace: "jscgs350", title: "Create a Thermostat Manager...", multiple: true)
             }
