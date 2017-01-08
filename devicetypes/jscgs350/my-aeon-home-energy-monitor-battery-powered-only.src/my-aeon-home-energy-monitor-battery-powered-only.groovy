f02e69d3422fa85855b88aa6e9430de49797b571
constjs <constjs@users.noreply.github.com>
2017-01-08 00:23:59 -0500

    Update my-aeon-home-energy-monitor-battery-powered-only.groovy

--- devicetypes/jscgs350/my-aeon-home-energy-monitor-battery-powered-only.src/my-aeon-home-energy-monitor-battery-powered-only.groovy
+++ devicetypes/jscgs350/my-aeon-home-energy-monitor-battery-powered-only.src/my-aeon-home-energy-monitor-battery-powered-only.groovy
@@ -1,5 +1,5 @@
 /**
- *  Aeon HEM1 (this DH version is set up for running on only batteries, no mains power)
+ *  Aeon HEM1 (this DH version is set up for running only on batteries, no mains power)
  *
  *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  *  in compliance with the License. You may obtain a copy of the License at:
