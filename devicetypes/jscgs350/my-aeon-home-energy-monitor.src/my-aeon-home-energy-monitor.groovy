537bd979925713779605a48817660f2a61b0034e
constjs <constjs@users.noreply.github.com>
2017-03-26 14:28:34 -0400

    Create my-aeon-home-energy-monitor.groovy

---
+++ devicetypes/jscgs350/my-aeon-home-energy-monitor.src/my-aeon-home-energy-monitor.groovy
@@ -0,0 +1,3 @@
+This has moved to:
+
+https://raw.githubusercontent.com/constjs/jcdevhandlers/master/devicetypes/jscgs350/my-aeon-home-energy-monitor-gen1.src/my-aeon-home-energy-monitor-gen1.groovy
