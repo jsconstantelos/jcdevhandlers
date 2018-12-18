90b18ec7230787bf738f14c88fd6140af33dfc0c
JOHN CONSTANTELOS <jsconst@gmail.com>
2018-12-18 12:12:26 -0500

    commit

---
+++ smartapps/jscgs350/my-button-controller-parent.src/my-button-controller-parent.groovy
@@ -0,0 +1,32 @@
+definition(
+    name: "My Button Controller Parent",
+    singleInstance: true,
+    namespace: "jscgs350",
+    author: "SmartThings",
+    description: "My Button Controller Parent",
+    category: "My Apps",
+    iconUrl: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience.png",
+    iconX2Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png")
+
+preferences {
+    page(name: "mainPage", title: "My Button Controller", install: true, uninstall: true,submitOnChange: true) {
+            section {
+                    app(name: "childRules", appName: "My Button Controller Child", namespace: "jscgs350", title: "Create a button controller...", multiple: true)
+            }
+    }
+}
+
+def installed() {
+    initialize()
+}
+
+def updated() {
+    unsubscribe()
+    initialize()
+}
+
+def initialize() {
+    childApps.each {child ->
+            log.info "Installed My Button Controller Child: ${child.label}"
+    }
+}
